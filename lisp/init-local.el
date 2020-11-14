4dae1934989c8fc3e6610a031d31a65fcda86b51
wangfan <wangfan@beyonds.com>
2020-11-14 23:51:58 +0800

    Add English translation plug

---
+++ lisp/init-local.el
@@ -0,0 +1,10 @@
+;;; init-local.el --- archer's config -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;; English translation plug
+(require 'insert-translated-name)
+
+
+(provide 'init-local)
+;;; init-local.el ends here
