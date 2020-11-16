12e95390fab61311a8f378a7e08c885b840b3cec
wangfan <wangfan@beyonds.com>
2020-11-16 21:10:59 +0800

    Org-mode configuration

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,6 +5,32 @@
 ;; English translation plug
 (require 'insert-translated-name)
 
+;; org-agenda
+(org-agenda-files '("~/org"))
+
+;; org-publish
+(require 'ox-publish)
+(setq org-publish-project-alist '(
+                                  ("org-notes"
+                                   :base-directory "~/org"
+                                   :base-extension "org"
+                                   :publishing-directory "~/public_html/"
+                                   :recursive t
+                                   :publishing-function org-html-publish-to-html
+                                   :headline-levels 4 ;; Just the default for this project.
+                                   )
+                                  ("org-static"
+                                   :base-directory "~/org"
+                                   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+                                   :publishing-directory "~/public_html/"
+                                   :recursive t
+                                   :publishing-function org-publish-attachment
+                                   )
+
+                                  ("org" :components ("org-notes" "org-static"))
+
+                                  )
+      )
 
 (provide 'init-local)
 ;;; init-local.el ends here
