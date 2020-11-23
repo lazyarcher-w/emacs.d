43f205f2b109daf1f0442a82db3ac813f49ed82a
wangfan <wnagfan@beyonds.com>
2020-11-23 14:01:43 +0800

    Reset org-default-notes-file

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -16,31 +16,7 @@
 (setq org-agenda-files '("~/org"))
 
 ;; Setting up org-capture
-(setq org-default-notes-file (concat org-directory "/inbox.org"))
-
-;; org-publish
-(require 'ox-publish)
-(setq org-publish-project-alist '(
-                                  ("org-notes"
-                                   :base-directory "~/org"
-                                   :base-extension "org"
-                                   :publishing-directory "~/public_html/"
-                                   :recursive t
-                                   :publishing-function org-html-publish-to-html
-                                   :headline-levels 4 ;; Just the default for this project.
-                                   )
-                                  ("org-static"
-                                   :base-directory "~/org"
-                                   :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
-                                   :publishing-directory "~/public_html/"
-                                   :recursive t
-                                   :publishing-function org-publish-attachment
-                                   )
-
-                                  ("org" :components ("org-notes" "org-static"))
-
-                                  )
-      )
+(setq org-default-notes-file (concat org-directory "/inbox.org.gpg"))
 
 ;; eaf
 ;; (when *is-a-linux*
