c6f8a86b0461e27c2a345acd788310fe3549e8c2
wangfan <wnagfan@beyonds.com>
2020-12-29 18:42:35 +0800

    Update theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -10,8 +10,8 @@
 (setq custom-safe-themes t)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
-(set-frame-parameter nil 'alpha '(85 . 100))
+(setq-default custom-enabled-themes '(sanityinc-solarized-light))
+;; (set-frame-parameter nil 'alpha '(85 . 100))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
