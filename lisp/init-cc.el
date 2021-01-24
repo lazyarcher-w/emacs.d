64cfdb3118ab44ea92982cdcf81f0b0dbe40ec6b
wangfan <wangfan@beyonds.com>
2021-01-24 11:42:26 +0800

    Use LSP to replace EGLOT and configure simple debug

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -2,14 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-
-(require-package 'eglot)
-(with-eval-after-load 'eglot
-  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
-  (add-hook 'c-mode-hook 'eglot-ensure)
-  (add-hook 'c++-mode-hook 'eglot-ensure)
-  )
-
+(add-hook 'c-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'lsp)
 
 (provide 'init-cc)
 ;;; init-cc.el ends here
--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -3,10 +3,10 @@
 ;;; Code:
 (require-package 'lsp-mode)
 (require-package 'lsp-ui)
-
+(require-package 'dap-mode)
 (setq lsp-keymap-prefix "C-c m")
 
-(setq lsp-enable-symbol-highlighting nil)
+(setq lsp-enable-symbol-highlighting t)
 (setq lsp-ui-doc-enable nil)
 (setq lsp-ui-doc-show-with-cursor nil)
 (setq lsp-ui-doc-show-with-mouse nil)
@@ -20,7 +20,37 @@
 (setq lsp-auto-guess-root t)
 (setq read-process-output-max (* 1024 1024))
 
-(add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+(defun archer/dap-cpptools-setup ()
+  ;; dap-mode configuration dap-cpptools
+  (require 'dap-cpptools)
+  (dap-cpptools-setup)
+  (dap-register-debug-template
+   "cpptools::(lldb) Launch current file"
+   (list :type "cppdbg"
+         :request "launch"
+         :name "cpptools::Run Configuration"
+         :MIMode "lldb"
+         :program "${fileDirname}/${fileBasenameNoExtension}"
+         :cwd "${workspaceFolder}"))
+  (defun archer/dap-debug-before-cc-compile-current-file ()
+    "Compile the current file before debug"
+    (interactive)
+    (defun archer/cc-compile-current-file-with-debug-flag ()
+      (let ((command (concat "cc -g " (buffer-name) " -o " (file-name-sans-extension (buffer-name)))))
+        (shell-command command)
+        (message (concat "build " (buffer-name) " finish, command: " command))))
+    (archer/cc-compile-current-file-with-debug-flag)
+    (dap-debug (list :type "cppdbg"
+                     :request "launch"
+                     :name "cpptools::Run Configuration"
+                     :MIMode "lldb"
+                     :program "${fileDirname}/${fileBasenameNoExtension}"
+                     :cwd "${workspaceFolder}"))))
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (yas-global-mode)
+  (archer/dap-cpptools-setup))
 
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
