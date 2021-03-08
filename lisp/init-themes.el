401679013ac2733c2bfe0dd407f7f5c6083594c0
wangfan <wnagfan@beyonds.com>
2021-03-08 18:37:33 +0800

    [lisp] init-themes.el: use doom-themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -4,14 +4,15 @@
 
 (require-package 'color-theme-sanityinc-solarized)
 (require-package 'color-theme-sanityinc-tomorrow)
+(require-package 'doom-themes)
 
 ;; Don't prompt to confirm theme safety. This avoids problems with
 ;; first-time startup on Emacs > 26.3.
 (setq custom-safe-themes t)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-solarized-light))
-(set-frame-parameter nil 'alpha 85)
+(setq-default custom-enabled-themes '(doom-one))
+(set-frame-parameter nil 'alpha 100)
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
@@ -30,13 +31,13 @@
 (defun light ()
   "Activate a light color theme."
   (interactive)
-  (setq custom-enabled-themes '(sanityinc-tomorrow-day))
+  (setq custom-enabled-themes '(doom-one-light))
   (reapply-themes))
 
 (defun dark ()
   "Activate a dark color theme."
   (interactive)
-  (setq custom-enabled-themes '(sanityinc-tomorrow-bright))
+  (setq custom-enabled-themes '(doom-one))
   (reapply-themes))
 
 
