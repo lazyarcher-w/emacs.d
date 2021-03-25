35f9179cbe14c1f35163aeea77150795ee81a8f9
wangfan <wnagfan@beyonds.com>
2021-03-25 18:29:26 +0800

    [init] lsp: Enable LSP in org babel

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -55,5 +55,47 @@
 
 (advice-add 'lsp :before (lambda (&rest _args) (eval '(setf (lsp-session-server-id->folders (lsp-session)) (ht)))))
 
+(setq centaur-lsp 'lsp-mode)
+(when centaur-lsp
+  ;; Enable LSP in org babel
+  ;; https://github.com/emacs-lsp/lsp-mode/issues/377
+  (cl-defmacro lsp-org-babel-enable (lang)
+    "Support LANG in org source code block."
+    (cl-check-type lang stringp)
+    (let* ((edit-pre (intern (format "org-babel-edit-prep:%s" lang)))
+           (intern-pre (intern (format "lsp--%s" (symbol-name edit-pre)))))
+      `(progn
+         (defun ,intern-pre (info)
+           (let ((file-name (->> info caddr (alist-get :file))))
+             (unless file-name
+               (user-error "LSP:: specify `:file' property to enable"))
+
+             (setq buffer-file-name file-name)
+             (pcase centaur-lsp
+               ('eglot
+                (and (fboundp 'eglot-ensure) (eglot-ensure)))
+               ('lsp-mode
+                (and (fboundp 'lsp-deferred) (lsp-deferred)))
+               (_ (user-error "LSP:: invalid `centaur-lsp' type")))))
+         (put ',intern-pre 'function-documentation
+              (format "Enable `%s' in the buffer of org source block (%s)."
+                      centaur-lsp (upcase ,lang)))
+
+         (if (fboundp ',edit-pre)
+             (advice-add ',edit-pre :after ',intern-pre)
+           (progn
+             (defun ,edit-pre (info)
+               (,intern-pre info))
+             (put ',edit-pre 'function-documentation
+                  (format "Prepare local buffer environment for org source block (%s)."
+                          (upcase ,lang))))))))
+
+  (defvar org-babel-lang-list
+    '("go" "python" "ipython" "ruby" "js" "css" "sass" "C" "rust" "java"))
+  (setq-local emacs/>=26p t)
+  (add-to-list 'org-babel-lang-list (if emacs/>=26p "shell" "sh"))
+  (dolist (lang org-babel-lang-list)
+    (eval `(lsp-org-babel-enable ,lang))))
+
 (provide 'init-lsp)
 ;;; init-lsp.el ends here
